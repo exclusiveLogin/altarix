c98f4f3821b85dc3ebafa9889f81d9ed4492e143
exclusiveLogin <alter4444@gmail.com>
2017-02-27 16:48:23 +0400

    v0.0.0 dev

---
+++ js/loader.js
@@ -0,0 +1,8 @@
+require("../style/dark.css");
+
+
+var app = require("angular");
+console.log(app);
+//var AAP = app.module("AltarixApp",[]);
+
+app.bootstrap(document,["AltarixApp"]);
---
+++ js/vendor.js
@@ -0,0 +1,6 @@
+require("jquery2");
+require("bootstrap");
+require("bootstrap/dist/css/bootstrap.min.css");
+require("bootstrap/dist/css/bootstrap-theme.min.css");
+require("font-awesome/css/font-awesome.min.css");
+window.Highcharts = require("highcharts");
