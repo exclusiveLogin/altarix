0f12ac0feec3ec519dc139dd13df247094d0ba9e
exclusiveLogin <alter4444@gmail.com>
2017-03-06 16:49:46 +0400

    before fix

--- js/vendor.js
+++ js/vendor.js
@@ -1,5 +1,10 @@
+//JQ--------------------------
 require("jquery2");
-//require("jquery-ui-dist/");
+require("jquery-ui-dist/jquery-ui.js");
+require("jquery-ui-dist/jquery-ui.css");
+//require("jquery-ui-dist/jquery-ui.structure.min.js");
+//require("jquery-ui-dist/jquery-ui.theme.min.js");
+//------------------------------
 require("bootstrap");
 require("bootstrap/dist/css/bootstrap.min.css");
 require("bootstrap/dist/css/bootstrap-theme.min.css");
